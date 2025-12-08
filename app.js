9a73efc5b2199403b9f2d8fa3b252e14e1e4b895
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-12-08 16:19:22 +0500

    i

---
+++ app.js
@@ -0,0 +1,40 @@
+var todo = document.getElementById("todo")
+var input = document.getElementById("input")
+
+function submit(){
+   var liTag = document.createElement("li") // tag new
+
+   //b tag create for store data 
+   var bTag = document.createElement("b")
+   bTag.innerText= input.value
+   bTag.style.marginTop="20px"
+
+   //button edit
+   var editButton = document.createElement("button")
+   editButton.innerText="EDIT"
+
+    var deleteButton = document.createElement("button")
+     deleteButton.innerText="DELETE"
+     deleteButton.style.marginLeft="20px"
+
+
+//    store b i li tag
+   liTag.appendChild(bTag)
+   liTag.appendChild(editButton)
+   liTag.appendChild(deleteButton)
+
+//    li tag store in main todo
+   todo.appendChild(liTag)
+   input.value=""
+
+}
+document.addEventListener("keydown",function(e){
+    if(e.key == "Enter"){
+        submit()
+    }
+
+
+})
+// = =>assign
+// ==>equal value
+// ==equal value or data type
