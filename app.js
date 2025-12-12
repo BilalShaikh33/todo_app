a83711c1b6fbe6f78c5dc168532a23f8deb8ce1c
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-12-12 17:18:17 +0500

    i

--- app.js
+++ app.js
@@ -12,29 +12,39 @@ function submit(){
    //button edit
    var editButton = document.createElement("button")
    editButton.innerText="EDIT"
-
-    var deleteButton = document.createElement("button")
-     deleteButton.innerText="DELETE"
-     deleteButton.style.marginLeft="20px"
-
-
-//    store b i li tag
+   
+   var deleteButton = document.createElement("button")
+   deleteButton.innerText="DELETE"
+   deleteButton.style.marginLeft="20px"
+   deleteButton.setAttribute("onclick", "deleteLi(this)")
+   
+   
+   //    store b i li tag
    liTag.appendChild(bTag)
    liTag.appendChild(editButton)
    liTag.appendChild(deleteButton)
-
-//    li tag store in main todo
+   
+   //    li tag store in main todo
    todo.appendChild(liTag)
    input.value=""
-
+   
 }
 document.addEventListener("keydown",function(e){
-    if(e.key == "Enter"){
-        submit()
-    }
-
-
+   if(e.key == "Enter"){
+      submit()
+   }
+   
+   
 })
+
+
+function deleteLi(event) {
+  console.log("test", event.parentNode.childNodes[0]);
+
+  event.parentNode.remove();
+}
+   
+
 // = =>assign
 // ==>equal value
 // ==equal value or data type
